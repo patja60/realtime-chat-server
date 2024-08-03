a98262a39ed6d283c3b08971f9091124d40ac8fe
Thanapat <thanapat.jutha@gmail.com>
2024-08-03 16:40:38 +0700

    UPDATE - implement template for main file logic

--- cmd/realtime-chat-server/main.go
+++ cmd/realtime-chat-server/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-    fmt.Println("Hello, World!")
+func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	// init db
+
+	// init service
+
+	// init handler -> inject service
+
+	// init router -> inject handler
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", HelloWorldHandler)
+	r.HandleFunc("/health-check", HealthCheckHandler)
+
+	// Start the server
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+
+}
