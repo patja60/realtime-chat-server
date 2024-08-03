35c310352267959e7c71631e3a34dc034a871e70
Thanapat <thanapat.jutha@gmail.com>
2024-08-03 17:22:22 +0700

    CREATE - create util function include hash and jwt

---
+++ pkg/auth/jwt.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("realtime-chat-secret")
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID string) (*string, error) {
+
+	claims := Claims{
+		userID,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * 24 * time.Hour)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		return nil, errors.New("unexpected signing key")
+	}
+
+	return &signedString, nil
+}
