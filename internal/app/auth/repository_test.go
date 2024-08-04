ac933e3bd0d0d45c6430becc3d873ba492defab3
Thanapat <thanapat.jutha@gmail.com>
2024-08-04 16:02:57 +0700

    CREATE - create test for repository include CreateUser and GetUserByEmail methods

--- internal/app/auth/repository_test.go
+++ internal/app/auth/repository_test.go
@@ -1,44 +1,115 @@
 package auth
 
-import "testing"
+import (
+	"database/sql"
+	"errors"
+	"testing"
 
-type mockDB struct {
-	users map[string]*User
+	"github.com/DATA-DOG/go-sqlmock"
+	database "github.com/patja60/realtime-chat-server/pkg"
+)
+
+func TestCreateUser(t *testing.T) {
+	t.Run("SuccessCreateUser", func(t *testing.T) {
+		sqlDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		defer sqlDB.Close()
+
+		db := &database.DB{Conn: sqlDB}
+
+		repo := NewAuthRepository(db)
+
+		email := "user@example.com"
+		hashedPassword := "hashedpassword"
+
+		mock.ExpectExec("INSERT INTO users").
+			WithArgs(email, hashedPassword).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err = repo.CreateUser(email, hashedPassword)
+
+		if err != nil {
+			t.Errorf("expected no error, but got %v", err)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+	})
 }
 
-func TestAuthRepository_Signup(t *testing.T) {
-	t.Run("Register", func(t *testing.T) {
-		repo := &mockAuthRepository{users: make(map[string]*User)}
-		usecase := NewAuthUsecase(repo)
+func GetUserByEmail(t *testing.T) {
+	t.Run("SuccessGetUserByEmail", func(t *testing.T) {
+		sqlDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		defer sqlDB.Close()
 
-		t.Run("Should have 1 user after signup", func(t *testing.T) {
-			email := "user@example.com"
-			err := usecase.Signup(email, "password123")
-			if err != nil {
-				t.Fatalf("Expected no error, got %v", err)
-			}
+		db := &database.DB{Conn: sqlDB}
 
-			if repo.users[email] == nil {
-				t.Fatalf("Expected user to be created, got error: %v", err)
-			}
+		repo := NewAuthRepository(db)
 
-			if len(repo.users) != 1 {
-				t.Fatalf("Expected to have 1 user to be created, got error: %v", err)
-			}
-		})
+		email := "user@example.com"
+		hashedPassword := "hashedpassword"
 
-		t.Run("DuplicateRegistration", func(t *testing.T) {
-			email := "user@example.com"
-			_ = usecase.Signup(email, "password123")
-			err := usecase.Signup(email, "password123")
-			if err == nil {
-				t.Fatalf("Expected error for duplicate registration, got nil")
-			}
+		rows := sqlmock.NewRows([]string{"id", "email", "password_hash"}).
+			AddRow("1", email, hashedPassword)
 
-			if len(repo.users) != 1 {
-				t.Fatalf("Expected to have 1 user to be created, got error: %v", err)
-			}
-		})
+		mock.ExpectQuery("SELECT id, email, passwordHash FROM user WHERE email = ?").
+			WithArgs(email).
+			WillReturnRows(rows)
 
+		user, err := repo.GetUserByEmail(email)
+
+		if err != nil {
+			t.Errorf("expected no error, but got %v", err)
+		}
+
+		if user.Email != email {
+			t.Errorf("expected email to be %v, but got %v", email, user.Email)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+	})
+
+	t.Run("NotExistUser", func(t *testing.T) {
+		sqlDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		defer sqlDB.Close()
+
+		db := &database.DB{Conn: sqlDB}
+
+		repo := NewAuthRepository(db)
+
+		email := "user@example.com"
+
+		mock.ExpectQuery("SELECT id, email, passwordHash FROM user WHERE email = ?").
+			WithArgs(email).
+			WillReturnRows(nil)
+
+		user, err := repo.GetUserByEmail(email)
+
+		if err == nil {
+			t.Errorf("expected an error, got nil")
+		}
+
+		if !errors.Is(err, sql.ErrNoRows) {
+			t.Errorf("expected sql.ErrNoRows, got %v", err)
+		}
+
+		if user != nil {
+			t.Errorf("expected user to be nil, got %v", user)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
 	})
 }
